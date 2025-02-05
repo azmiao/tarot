81169eae35a78d51ec953fa714eef5f6fde79216
azmiao <2362020227@qq.com>
2025-02-05 11:10:34 +0800

    优化塔罗牌代码

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+from yuiChyan import CQEvent
 from yuiChyan.config import NICKNAME
 
 config_path = os.path.join(os.path.dirname(__file__), 'config.json')
@@ -14,25 +15,23 @@ meanings = {
 
 
 # 读取配置
-async def read_config():
+async def read_config() -> dict:
     with open(config_path, 'r', encoding='utf-8') as f:
         config = json.load(f)
     return config
 
 
 # 生成消息
-async def chain_reply(ev, chain, msg, image):
-    msg = ''.join(msg)
-    data = {
-        'type': 'node',
-        'data': {
-            'name': str(NICKNAME),
-            'uin': str(ev.self_id),
-            'content': [
-                {'type': 'text', 'data': {'text': msg}},
-                {'type': 'image', 'data': {'file': image}}
-            ]
+async def generate_forward_msg(ev: CQEvent, msg_list: list) -> list[dict]:
+    data_list = []
+    for msg in msg_list:
+        data = {
+            'type': 'node',
+            'data': {
+                'name': str(NICKNAME),
+                'uin': str(ev.self_id),
+                'content': msg
+            }
         }
-    }
-    chain.append(data)
-    return chain
+        data_list.append(data)
+    return data_list
